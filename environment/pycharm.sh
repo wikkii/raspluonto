2a432d06d07b222cb0c6dec3ee8aa760dcd92d87
wikkii <wikkiia@gmail.com>
2017-09-04 14:36:05 +0000

    pycharm installation and launch script

---
+++ environment/pycharm.sh
@@ -0,0 +1,14 @@
+mkdir pycharm
+
+cd pycharm
+
+wget "https://download.jetbrains.com/python/pycharm-community-2017.2.2.tar.gz"
+
+tar -xf pycharm-community-2017.2.2.tar.gz 
+
+cd pycharm-community-2017.2.2/
+
+cd bin/
+
+./pycharm.sh
+
