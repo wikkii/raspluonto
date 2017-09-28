30bd153216ea7e5dd52000c0e7e9afdbc6b78e8e
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-09-28 16:41:36 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -15,6 +15,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
+
 }
 
 echo "Connected successfully";
