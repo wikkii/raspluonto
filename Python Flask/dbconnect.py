f5ffd6ab0c878112c9c8e6b1dfa18e198e27450f
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-10-06 23:39:33 +0300

    Update dbconnect.py

--- Python Flask/dbconnect.py
+++ Python Flask/dbconnect.py
@@ -3,10 +3,10 @@ import MySQLdb
 def connection():
 
 	#Insert your actual values here
-	conn = MySQLdb.connect(host="localhost",
-							user="username",
-							passwd="password",
-							db="nuotiovahti")
+	conn = MySQLdb.connect(host = "localhost",
+				user = "username",
+				passwd = "password",
+				db = "nuotiovahti")
 							
 	c = conn.cursor()
 	
