a4df820abc984ad31225f3fdd33e916b5f117f78
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-10-11 14:32:07 +0300

    Create connect_ssh.sh

---
+++ RemoteSSH/connect_ssh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+createTunnel() {
+	/usr/bin/ssh -N -R 2222:localhost:22 markus@139.59.140.158
+
+
+	if [[ $? -eq 0 ]]; then
+		echo Tunnel created successfully
+	else
+		echo An error occured while creating a tunnel. RC was $?
+	fi
+}
+
+	/bin/pidof ssh
+	
+	if [[ $? -ne 0 ]]; then
+		echo Creating new tunnel connection
+	
+		createTunnel
+	
+fi
