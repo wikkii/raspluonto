3a7e190a0a461eb37b02dc495d9c0b661379f7a7
wikkii <wikkiia@gmail.com>
2017-10-11 11:13:18 +0000

    flame test for local use

---
+++ sensor_testing/noMQTT_flame_test.py
@@ -0,0 +1,28 @@
+# ky_026_flame.py - report presence of IR light from flame to serial
+# (c) BotBook.com - Karvinen, Karvinen, Valtokari
+# modified 6 Oct 2017
+
+#-*- coding: utf-8 -*-
+
+import time
+import botbook_gpio as gpio
+
+
+def main():
+
+	flamePin = 8
+	gpio.mode(flamePin, "in")
+	#small wait before starting
+	print ("starting flame sensor...")
+	time.sleep(0.5)
+	
+	while(True):
+		flame = gpio.read(flamePin)
+		if(flame == gpio.LOW):
+			print ("Flame detected " + time.ctime())
+		else:
+			print ("NO flame detected " + time.ctime())
+		time.sleep(1)
+
+if __name__ == "__main__":
+main()
