8071cd1c20a2b6d7c486439359b6d5e922debcda
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-10-13 09:53:48 +0000

    Update startsensors.sh

--- Automation/startsensors.sh
+++ Automation/startsensors.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #This script will be run by cron after starup. 
 
-       python /test_code/mqtt_pir_test.py & python /test_code/mqtt_flame.py
-
+       python /home/pi/test_code/mqtt_pir_test.py & python /home/pi/test_code/mqtt_flame.py
