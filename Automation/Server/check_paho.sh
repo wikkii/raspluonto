cdc6c68f970e68391a919791ef6f6329e0fc8fb3
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-10-27 19:32:05 +0300

    Keeps paho-mqtt alive. Run this every 10 minutes.

---
+++ Automation/Server/check_paho.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Checks whether paho-mqtt-client is running or not. If it isn't, then restart it.
+# This script is run every 10 minutes by cron.
+ 
+output=$(pgrep -fn paho-mqtt-client.py)
+testi=$output
+ 
+if [ "$testi" ]; then
+:
+else
+python /home/markus/paho-mqtt/paho-mqtt-client.py >/dev/null &
+fi
