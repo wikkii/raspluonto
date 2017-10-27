36a4fd77b07df4c33a906914c346a118915fbcc9
wikkii <wikkiia@gmail.com>
2017-10-27 12:43:10 +0300

    added mysql_mqtt.py
    inserts received mqtt messages into database

---
+++ Python Flask/public_html/nuotiovahti/nuotiovahti/mysql_mqtt.py
@@ -0,0 +1,52 @@
+import paho.mqtt.client as mqtt
+import mysql.connector
+from mysql.connector import errorcode
+
+#cursor = cnx.cursor()
+
+def addtobase(message):
+        try:
+                cnx = mysql.connector.connect(user='nuotiovahti', password='nuotiovahti666!',
+                                                host='127.0.0.1',
+                                                database='nuotiovahti')
+        except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                        print("Something is wrong with your user name or password")
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                        print("Database does not exist")
+                else:
+                        print(err)
+        else:
+                print("connected to database")
+
+
+        cursor = cnx.cursor()
+        print(message + "adding")
+        add_sensordata = ("INSERT INTO sensordata VALUES(%(data)s)")
+
+        data_sensordata = {
+                'data': message
+        }
+
+        cursor.execute(add_sensordata, data_sensordata)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
+def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("nuotiovahti")
+def on_message(client, userdata, msg):
+        #if msg.payload.decode() == "Hello world!":
+        addtobase(msg.payload)
+        print(msg.payload + " hello")
+        #client.disconnect()
+
+
+client = mqtt.Client()
+client.connect("127.0.0.1",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever(
