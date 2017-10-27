2c170d4d449df698a89ba2a36db3c3eaf69b7991
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-10-27 19:39:28 +0300

    Update and rename Automation/remoteconnection.sh to Automation/Raspberry/remoteconnection.sh

--- Automation/Raspberry/remoteconnection.sh
+++ Automation/Raspberry/remoteconnection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Script for creating a reverse SSH tunnel from Raspberry Pi to the server
+# Script for creating a reverse SSH tunnel from Raspberry Pi to the server
 
         ssh -f -N -o BatchMode=yes -R 2222:localhost:22 markus@139.59.140.158
