d35e6c368db28608d0823caa94793deccf9c5fde
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-11-02 23:21:54 +0200

    Update main.js

--- public_html/AJAX/main.js
+++ public_html/AJAX/main.js
@@ -2,7 +2,6 @@ var testContainer = document.getElementById("tabletest");
 
 var pageRequest = new XMLHttpRequest();
 pageRequest.open('GET', 'PirSensor.json');
-//pageRequest.open('GET', 'FlameSensor.json');
 pageRequest.onload = function() {
 
 	var myTestData = JSON.parse(pageRequest.responseText);
@@ -16,30 +15,41 @@ function renderTest(data) {
 	var htmlString3 = "";
 
 
-
 	if (data.length > 0) {
 
 		for (i = 0; i < data.length; i++) {
 			
-			
-			//j = 0
 
-			//if (data[i].pir_rows > 3) {
+			htmlString3 += "<tr><td>" + data[i].location + "</td><td class='AreaStatusNo'>" + data[i].pir_rows + "</td><td>" + data[i].flame_rows + "</td></tr>";
 
-				//j = 1
-			//}
-
-
-			htmlString3 += "<tr><td>" + data[i].location + "</td><td>" + data[i].pir_rows + "</td><td>" + data[i].flame_rows + "</td></tr>";
-
-			if (data[i].pir_rows < 10) {
-				document.getElementsByTagName("td")[1].innerHTML = "Varattu";
+			if (data[i].pir_rows < 5) {
 				
-		}
+				console.log ("yo V1")
+
+				$( "th" ).text("Hello");
+				/*$('.AreaStatusNo').replaceWith('.hello');
+				$('.hello').text("Hello World!");*/
+				//$( ".AreaStatusNo" ).css("background-color", "blue");
+				
+				$( ".AreaStatusNo" ).css( "color", "red" )
+ 				.add( "p" )
+  				.css( "background", "orange" );				
+
+// https://api.jquery.com/add/
+			}
+
+
+			else	{
+
+				console.log ("yo V2")
+			}
+
+
+			
 
 		}
 
-	console.log ("yo" + htmlString3)
+	console.log ("yo V3" + htmlString3)
 	testContainer.insertAdjacentHTML('beforeend', htmlString3)
 
 
@@ -59,15 +69,11 @@ function renderTest(data) {
 
 
 /*
-
 if (data[i].status == 1) {
-
 		document.getElementsByTagName("th")[1].style.backgroundColor = "Yellow";
 		document.getElementsByTagName("th")[1].innerHTML = "Varattu";
 		
 		}
-
-
 		else {
 		document.getElementsByTagName("th")[1].style.backgroundColor = "orange";
 		document.getElementsByTagName("th")[1].innerHTML = "Tyhja";
@@ -75,11 +81,7 @@ if (data[i].status == 1) {
 	
 		}
 	}
-
 	testContainer.insertAdjacentHTML('beforeend', htmlString3)
 	
 }
-
-
 */
-
