7dd45eb5a27717d85c64b5cdac5198c11bf3e40b
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-11-07 21:14:43 +0200

    Update main.js

--- public_html/AJAX/main.js
+++ public_html/AJAX/main.js
@@ -1,50 +1,48 @@
 /*
-======================
+============================================
 JavaScript for AJAX request.
 Gets and handles JSON data from the server.
-
-
 Virtuaaliluonto 3.11.2017
-======================
+============================================
 */
 
 
 // Points to a table class in HTML
 var dataContainer = document.getElementById("datatable");
 
-// Request & receive JSON Data
-var pageRequest = new XMLHttpRequest();
-pageRequest.open('GET', '/static/json/sensor_data.json');
-pageRequest.onload = function() {
+function getData () {
+
+
+	// Request & receive JSON Data
+	var pageRequest = new XMLHttpRequest();
+	pageRequest.open('GET', 'sensor_data.json');
+	pageRequest.onload = function() {
 	
-	// Save JSON data to a variable
-	var mySensorData = JSON.parse(pageRequest.responseText);
-	// Call the renderTest function and pass it to mySensorData variable
-	renderData(mySensorData);
-};
+
+		
+		// Save JSON data to a variable
+		var mySensorData = JSON.parse(pageRequest.responseText);
+
+		// Call the renderTest function and pass it to mySensorData variable
+		renderData(mySensorData);
+	};
+
 
 // Send the request
-
-/*
-function proxy()
-{
-  pageRequest.send();
-}
-
-setInterval( proxy, 10000 ); // last arg is in milliseconds
-
-
-*/
-
-
 pageRequest.send();
+
+} // Function ends here
+ 
+	// Call function before setting an interval
+	getData();
+	// Set Interval. Last argument is in milliseconds NOTE: setInterval() keeps triggering expression again and again unless you tell it to stop
+	setInterval( getData, 10000 ); // Try setTimeout as well.
 
 
 // Add HTML to the page. Array of objects is accessed with "data".
 function renderData(data) {
 	
 	var htmlString = "";
-
 
 	if (data.length > 0) {
 
@@ -72,10 +70,10 @@ function renderData(data) {
 			htmlString += "<tr><td>" + data[i].location + "</td>"+ pirhtml + data[i].pir_rows + "</td>" + flamehtml + data[i].flame_rows + "</td></tr>";
 
 
-
 		}
 
 	// Add htmlString as content to HTML
+
 	dataContainer.insertAdjacentHTML('beforeend', htmlString)
 				
 				// Text values for the classes
@@ -88,7 +86,24 @@ function renderData(data) {
 
 				$( ".FlameStatusYes" ).text("Fire!");
 
+
 	}
 
 
 }
+
+
+// PS. KATSO: https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
+		//https://stackoverflow.com/questions/5215181/proper-way-to-handle-304-not-modified-in-jquery-ajax
+
+
+	//dataContainer.insertAdjacentHTML('afterend', "")
+
+
+	//$(htmlString).html("");
+
+	//dataContainer.html("");
+
+	//var htmlStringEmpty = "";
+
+	//$("#datatable").html(htmlstringEmpty);
