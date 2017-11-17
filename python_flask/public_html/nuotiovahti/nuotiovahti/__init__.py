34f1d27e95d737c6678ba739be8527d9983bc1b7
wikkii <wikkiia@gmail.com>
2017-11-17 15:50:46 +0200

    updated init.py

--- python_flask/public_html/nuotiovahti/nuotiovahti/__init__.py
+++ python_flask/public_html/nuotiovahti/nuotiovahti/__init__.py
@@ -7,36 +7,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+        return render_template('index.html')
 
 
 @app.route('/data')
 def getData():
-	try:
-	        cnx = mysql.connector.connect(option_files='connector.cnf')
-	except mysql.connector.Error as err:
-	        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-	                print("Something is wrong with your user name or password")
-	        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-	                print("Database does not exist")
-	        else:
-	                print(err)
-	else:
-	        print("connected to database")
+        cnx=0
+        try:
+                cnx = mysql.connector.connect(user='nuotiovahti', password='pass', host='127.0.0.1, database='nuotiovahti'
+        except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                        print("Something is wrong with your user name or password")
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                        print("Database does not exist")
+                else:
+                        print(err)
+      
+          
 
 
-	cursor = cnx.cursor()
-	print("gettin data damn it...")
+        cursor = cnx.cursor()
+       
 
 
-	cursor.execute("select (select count(f.detection_time) from Flame as f where detection_time >now()- INTERVAL 300 SECOND) as flame, (select count(p.detection_time) from Pir as p where detection_time >now()- INTERVAL 300 SECOND) as pir;")
+        cursor.execute("select (select count(f.detection_time) from Flame as f where detection_time >now()- INTERVAL$
 
-	datarows = jsonify(cursor.fetchall())
+        datarows = jsonify(cursor.fetchall())
 
-	cursor.close()
-	cnx.close()
+        cursor.close()
+        cnx.close()
 
-	return datarows
+        return datarows
 
 
 if __name__ == "__main__":
