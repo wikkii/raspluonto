5fef07592d902a46d2916336384528d9d3d96e17
wikkii <wikkiia@gmail.com>
2017-11-19 19:33:36 +0200

    comments added

--- python_flask/public_html/nuotiovahti/nuotiovahti/__init__.py
+++ python_flask/public_html/nuotiovahti/nuotiovahti/__init__.py
@@ -4,15 +4,17 @@ from flask import Flask, render_template, jsonify, json, request
 app = Flask(__name__)
 
 
-
+#home page url
 @app.route("/")
 def main():
         return render_template('index.html')
 
-
+#sensor data url, default nethod is GET
 @app.route('/data')
 def getData():
-        cnx=0
+        #initialize cnx to stop error
+        cnx=0 
+        #connecting to database
         try:
                 cnx = mysql.connector.connect(user='nuotiovahti', password='pass', host='127.0.0.1, database='nuotiovahti'
         except mysql.connector.Error as err:
@@ -24,13 +26,14 @@ def getData():
                         print(err)
 
         cursor = cnx.cursor()
-  
+        #getting sensor data from the database, gets the last 300 seconds of data
         cursor.execute("select (select count(f.detection_time) from Flame as f where detection_time >now()- INTERVAL 300 SECOND) as flame, (select count(p.detection_time) from Pir as p where detection_time >now()- INTERVAL 300 SECOND) as pir;")
+        #turning data into json and saving it into datarows
         datarows = jsonify(cursor.fetchall())
-
+        #close cursor and database connection
         cursor.close()
         cnx.close()
-
+        #return json data 
         return datarows
 
 
