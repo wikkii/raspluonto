0d1c96d1ad7954a80f977de53b1a74dc5233b75e
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-12-07 01:30:21 +0200

    Create killsshd.sh

---
+++ automation/Server/killsshd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CheckPID=$(sudo lsof -ti TCP:2222)
+PID=$CheckPID
+
+CheckSession=$(who)
+session=$CheckSession
+
+#CheckSession = $(ps aux | grep '[m]arkus@pts/0')
+#session = $CheckSession
+
+
+if [ "$session" ]; then
+#:
+echo 'Someone is logged in! Aborting...'
+
+else
+#ps aux | grep 'sshd: markus' | awk '{print $2}' | grep -v $PID | sudo xargs kill -9
+echo 'Just killed all processes!'
+
+fi
