a6175198c4a47c6e39abcbe5c07b9312ac70eae5
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-12-11 17:28:47 +0200

    Update remoteconnection.sh

--- automation/Raspberry/remoteconnection.sh
+++ automation/Raspberry/remoteconnection.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # Script for creating a reverse SSH tunnel from Raspberry Pi to the server
 
-        ssh -f -N -o BatchMode=yes -R 2222:localhost:22 markus@139.59.140.158
+#output=$(netstat | awk '$5 ~ /^139.59.140.158:ssh/ && $6 ~ /ESTABLISHED/')     # NOT RELIABLE AS IT IS! Just run the ssh command by itself.
+#connection=$output
+
+#if [ "$connection" ]; then
+#:
+#else
+ssh -f -N -o BatchMode=yes -R 2222:localhost:22 markus@139.59.140.158
+#fi
