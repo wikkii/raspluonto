8f2b784e48f92bb831828bb27ff6f479641c8443
PyhaMarkus <markus.pyharanta@myy.haaga-helia.fi>
2017-12-15 23:23:46 +0200

    Update killconnections.sh

--- automation/Server/killconnections.sh
+++ automation/Server/killconnections.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Script for killing old connections to port 2222. Run this after remote connection is over.
-# Note! This is run on the server! You can check if (CLOSE WAIT) connections are still alive with: "sudo lsof -i TCP:2222".
+# Script for killing old connections to port 2222. Run this after Raspberry Pi has been restarted.
 
-sudo lsof -t -i tcp:2222 -s tcp:listen | sudo xargs kill
+  sudo lsof -t -i tcp:2222 -s tcp:listen | sudo xargs kill
